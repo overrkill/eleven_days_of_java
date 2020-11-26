7ed6401953612a9467db8d034386432f508fac76
overrkill <zabhishekkale@gmail.com>
2020-11-26 12:55:38 +0530

    Day 1  - Scopes

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    String iv = "Instance Variable";
+    // Scopes
+    // Instance Variable needs to have an object or static
+    Integer a = Integer.valueOf("1"); // this works
+    public  void abc(){
+        String iv = "Method variable";
+        System.out.println(iv);
+    }
+    public static void main(String[] args) {
+    //Basic User I/O
+        Scanner reader =  new Scanner(System.in);
+        int a = reader.nextInt();
+        System.out.println(a);
+	// write your code here
+        Main o1 = new Main();
+        o1.abc();
+        System.out.println(o1.iv);
+        System.out.println("Hello World!" + o1.iv);
+        for (int i = 0;i<10;){
+            System.out.println((++i)+" Hello");
+        }
+
+    }
+}
